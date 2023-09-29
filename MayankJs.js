47d1d4311155ca6fa371d353b7c8a4c7650828d5
Mayank <mayanksakat333@gmail.com>
2023-09-29 20:46:58 +0530

    My First Commit

---
+++ MayankJs.js
@@ -0,0 +1,21 @@
+// setInterval(function(){
+// alert("Hi");
+// },5000)
+
+let ybtn = document.getElementById("ybtn")
+let gbtn = document.getElementById("gbtn")
+let bbtn = document.getElementById("bbtn")
+let rbtn = document.getElementById("rbtn")
+
+ybtn.onclick = ()=> {
+    alert("You Have Clicked on yellow button")
+}
+gbtn.onclick = ()=> {
+    alert("You Have Clicked on green button")
+}
+bbtn.onclick = ()=> {
+    alert("You Have Clicked on blue button")
+}
+rbtn.onclick = ()=> {
+    alert("You Have Clicked on red button")
+}
